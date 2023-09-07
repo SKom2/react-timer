dd31636474f7913219b73ae90fc6dbfacee5b600
Aleksandr Komolkin <san4es.kom@gmail.com>
2023-09-07 21:23:01 +0300

    initial (bad) functionality of setting time value

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -2,16 +2,42 @@ import './Timer.scss';
 import { useEffect, useState } from "react";
 
 export default function Timer() {
-    const [time, setTime] = useState(5);
+    const [time, setTime] = useState(0);
     const [hours, setHours] = useState(0);
     const [minutes, setMinutes] = useState(0);
     const [seconds, setSeconds] = useState(0);
     const [isRunning, setIsRunning] = useState(false);
     const [intervalId, setIntervalId] = useState(null);
     const [isStopButton, setIsStopButton] = useState(true)
+    const [isTimingsState, setIsTimingsState] = useState(false)
+
+    function goOnTimingTable(){
+        if (isRunning) return;
+        setIsTimingsState(true);
+    }
+
+    function chooseTiming(e){
+        console.log(e.target.textContent.replace(/\d+/g, "").trim())
+        const unitsOfTime = e.target.textContent.replace(/\d+/g, "").trim();
+        const time = parseInt(e.target.textContent)
+        if (unitsOfTime === 'sec'){
+            setSeconds(time);
+            setTime(time)
+        }
+        if (unitsOfTime === 'min'){
+            setMinutes(time)
+            setTime(time * 60)
+        }
+        if (unitsOfTime === 'hour'){
+            setHours(time)
+            setTime(time * 3600)
+        }
+        setIsTimingsState(false)
+    }
 
     function startTimer() {
         if (isRunning) return;
+        if (time === 0) return;
         setIsRunning(true);
 
         const endTime = time + Math.floor(Date.now() / 1000);
@@ -56,23 +82,40 @@ export default function Timer() {
         setHours(0)
         setMinutes(0);
         setSeconds(0);
+        setTime(0)
     }
 
     return (
         <section className='timer'>
-            <h1 className='timer__title'>Timer</h1>
+            <h1 className='timer__title'>Simple Timer</h1>
             <div className="timer__container">
                 <p className="timer__values hours">{hours} hours</p>
                 <p className="timer__values minutes">{minutes} min</p>
                 <p className="timer__values seconds">{seconds} sec</p>
             </div>
             <div className="timer__buttons">
-                {isStopButton ?
-                    <button className="timer__button cancel-button" onClick={stopTimer}>Stop</button>
-                    :
-                    <button className="timer__button reset-button" onClick={resetTimer}>Reset</button>
+                {!isTimingsState ? (
+                        <>
+                            {isStopButton ?
+                                <button className="timer__button cancel-button" onClick={stopTimer}>Stop</button>
+                                :
+                                <button className="timer__button reset-button" onClick={resetTimer}>Reset</button>
+                            }
+                                <button className="timer__button time-button" onClick={goOnTimingTable}>Choose time</button>
+                                <button className="timer__button start-button" onClick={startTimer}>Start</button>
+                        </>
+                    ) : (
+                        <>
+                            <button className="timer__button timing-button" onClick={chooseTiming}>15 sec</button>
+                            <button className="timer__button timing-button" onClick={chooseTiming}>1 min</button>
+                            <button className="timer__button timing-button" onClick={chooseTiming}>5 min</button>
+                            <button className="timer__button timing-button" onClick={chooseTiming}>15 min</button>
+                            <button className="timer__button timing-button" onClick={chooseTiming}>30 min</button>
+                            <button className="timer__button timing-button" onClick={chooseTiming}>1 hour</button>
+                        </>
+                    )
                 }
-                <button className="timer__button start-button" onClick={startTimer}>Start</button>
+
             </div>
         </section>
     )
