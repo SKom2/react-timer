b9b1789f3277809ce13c92b01b1149096bf0a3c7
Aleksandr Komolkin <san4es.kom@gmail.com>
2023-09-10 13:07:55 +0300

    added timer signal

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,6 +1,7 @@
 import './Timer.scss';
 import { useEffect, useState } from "react";
 import {useForm} from "../../hooks/useForm";
+import sound from '../../sounds/signal-elektronnogo-budilnika-33304.mp3'
 
 export default function Timer() {
     const [time, setTime] = useState(0);
@@ -15,8 +16,10 @@ export default function Timer() {
     const [isFormAvailable, setIsFormAvailable] = useState(false)
     const [remainingTime, setRemainingTime] = useState(0)
     const [endTime, setEndTIme] = useState(0)
+    const [isSignalActive, setIsSignalActive] = useState(false)
+    const [signal] = useState(new Audio(sound))
 
-    const {values, handleChange, errors, isValid, setIsValid, resetForm} = useForm({
+    const {values, handleChange, resetForm} = useForm({
         hours: '',
         minutes: '',
         seconds: ''
@@ -42,7 +45,9 @@ export default function Timer() {
                 clearInterval(id);
                 setIsRunning(false);
                 setIsStopButton(false)
+                setIsInProcess(false)
                 displayTime(0)
+                playSignal()
                 return;
             }
 
@@ -73,13 +78,13 @@ export default function Timer() {
     }
 
     function resetTimer(){
+        stopSignal()
         setIsStopButton(false);
         setIsRunning(false);
         setIsInProcess(false)
         setHours(0)
         setMinutes(0);
         setSeconds(0);
-        console.log(initialTime)
         setTime(initialTime)
         clearInterval(intervalId)
         displayTime(initialTime)
@@ -98,35 +103,54 @@ export default function Timer() {
         displayTime(totalTime)
     }
 
+    function playSignal() {
+        signal.play();
+        setIsSignalActive(true)
+    }
+
+    function stopSignal(){
+        signal.pause();
+        signal.currentTime = 0;
+        setIsSignalActive(false)
+    }
+
+
     return (
         <section className='timer'>
             <h1 className='timer__title'>Simple Timer</h1>
             {isFormAvailable ? (
                 <form className="timer__container">
-                    <input
+                    <label className='timer__values-label'><input
                         type='number'
                         className="timer__values hours"
-                        placeholder='00 hours'
+                        placeholder='0'
                         name='hours'
                         value={values.hours || ''}
                         onChange={handleChange}
-                    ></input>
-                    <input
-                        type='number'
-                        className="timer__values minutes"
-                        placeholder='00 minutes'
-                        name='minutes'
-                        value={values.minutes || ''}
-                        onChange={handleChange}
-                    ></input>
-                    <input
-                        type='number'
-                        className="timer__values seconds"
-                        placeholder='00 secs'
-                        name='seconds'
-                        value={values.seconds || ''}
-                        onChange={handleChange}
-                    ></input>
+                    /> hours</label>
+
+                    <label className='timer__values-label'>
+                        <input
+                            type='number'
+                            className="timer__values minutes"
+                            placeholder='0'
+                            name='minutes'
+                            value={values.minutes || ''}
+                            onChange={handleChange}
+                        ></input>
+                        min
+                    </label>
+                    <label className='timer__values-label'>
+                        <input
+                            type='number'
+                            className="timer__values seconds"
+                            placeholder='0'
+                            name='seconds'
+                            value={values.seconds || ''}
+                            onChange={handleChange}
+                        ></input>
+                        sec
+                    </label>
                 </form>
             ) : (
                 <div className="timer__container" onClick={() => {
@@ -134,25 +158,34 @@ export default function Timer() {
                     resetForm()
                     setIsFormAvailable(true)
                 }}>
-                    <p className="timer__values hours">{hours} hours</p>
-                    <p className="timer__values minutes">{minutes} min</p>
-                    <p className="timer__values seconds">{seconds} sec</p>
+                    <label className='timer__values-label'>
+                        <p className="timer__values hours">{hours}</p>
+                        hours
+                    </label>
+                    <label className='timer__values-label'>
+                        <p className="timer__values minutes">{minutes}</p>
+                        min
+                    </label>
+                    <label className='timer__values-label'>
+                        <p className="timer__values seconds">{seconds}</p>
+                        sec
+                    </label>
                 </div>
             )}
             <div className="timer__buttons">
                 {isFormAvailable ? (
                     <>
                         <button className="timer__button cancel-button" onClick={() => setIsFormAvailable(false)}>Cancel</button>
-                        <button type="submit" className="timer__button submit-button" onClick={handleSubmit}>Submit</button>
+                        <button type="submit" className="timer__button submit-button" onClick={handleSubmit} disabled={!values.hours && !values.minutes && !values.seconds}>Submit</button>
                     </>
                 ) : (
                     <>
                         <button className="timer__button reset-button" onClick={resetTimer}>Reset</button>
                         {!isStopButton ?
                             !isInProcess ?
-                                <button className="timer__button start-button" onClick={startTimer}>Start</button>
+                                <button className="timer__button start-button" onClick={startTimer} disabled={isSignalActive}>Start</button>
                                 :
-                                <button className="timer__button start-button" onClick={startTimer}>Continue</button>
+                                <button className="timer__button start-button" onClick={startTimer}>Resume</button>
                             :
                             (<button className="timer__button stop-button" onClick={stopTimer}>Stop</button>)
                         }
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -2,35 +2,25 @@ import {useCallback, useState} from "react";
 
 export const useForm = (initialState) => {
     const [values, setValues] = useState(initialState);
-    const [errors, setErrors] = useState({});
-    const [isValid, setIsValid] = useState(false);
 
     function handleChange(evt){
         const input = evt.target;
         const name = input.name;
         const value = input.value;
         setValues({...values, [name]: value})
-        setErrors({...errors, [name]: input.validationMessage});
-        setIsValid(input.closest('form').checkValidity());
     }
 
     const resetForm = useCallback(
-        (newValues = {}, newErrors = {}, newIsValid = false) => {
+        (newValues = {}) => {
             setValues(newValues);
-            setErrors(newErrors);
-            setIsValid(newIsValid);
         },
-        [setValues, setErrors, setIsValid]
+        [setValues]
     );
 
     return {
         values,
-        errors,
-        isValid,
         handleChange,
         setValues,
-        setIsValid,
-        setErrors,
         resetForm
     };
 }
